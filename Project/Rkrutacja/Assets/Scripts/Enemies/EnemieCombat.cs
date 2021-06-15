e739792e21f8e6435c10960636f7e98b8192b50b
Niewiar <patrykniewiarowski@gmail.com>
2021-06-15 15:47:21 +0200

    enemy repulse

---
+++ Project/Rkrutacja/Assets/Scripts/Enemies/EnemieCombat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieCombat : MonoBehaviour
+{
+    [Header("Repulse target")]
+    [SerializeField] private bool _canRepulseTarget;
+    [SerializeField] private float _horizontalRepulseForce = 100f;
+    [SerializeField] private float _verticalRepulseForce = 500f;
+    [SerializeField] private float _timeToUnlockTargetMovement = 0.7f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            RepulseTarget(collision.gameObject);
+        }
+    }
+
+    private void RepulseTarget(GameObject target)
+    {
+        if (_canRepulseTarget)
+        {
+            Vector2 direction = target.transform.position - transform.position;
+            direction = direction.normalized;
+            target.GetComponent<Rigidbody2D>().AddForce(new Vector2(direction.x * _horizontalRepulseForce, direction.y * _verticalRepulseForce));
+        }
+    }
+}
